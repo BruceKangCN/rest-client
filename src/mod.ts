87a6282116b4a4ed1180b42dacdf480467d47be1
Bruce Kang <brucekangcn@gmail.com>
2025-05-01 11:08:31 +0800

    feat: impl OPTIONS, CONNECT, TRACE methods

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,8 @@
 import { deepMerge } from "@cross/deepmerge";
 
+/**
+ * HTTP request methods
+ */
 export type HTTPMethod =
     | "GET"
     | "HEAD"
@@ -11,8 +14,15 @@ export type HTTPMethod =
     | "TRACE"
     | "PATCH";
 
+/**
+ * error throwed on error response (e.g. response with status code of 403 or 500)
+ * @see RESTClient
+ */
 export class RESTError extends Error {
+    /** REST request coresponding to the response */
     request: Request;
+
+    /** REST response that respond the error */
     response: Response;
 
     constructor(req: Request, res: Response) {
@@ -25,10 +35,29 @@ export class RESTError extends Error {
     }
 }
 
+/**
+ * A REST request client.
+ *
+ * uses fetch API to send requests and get responses.
+ *
+ * on request, it automatically serialize request body to JSON format and set
+ * "Content-Type" to "application/json".
+ *
+ * on response, if success and response is ok, it automatically parse response
+ * body to object. if response is not ok, `RESTError` is throwed. for "HEAD"
+ * request, response body is ignored and `undefined` is returned.
+ *
+ * errors during fetch (e.g. `AbortError`, `NotAllowedError`) are throwed as-is.
+ *
+ * @see RESTError
+ */
 export class RESTClient {
     baseURL: string;
     defaultOptions: RequestInit;
 
+    /**
+     * construct a REST client with optional base URL and default options.
+     */
     constructor(baseURL?: string, defaultOptions?: RequestInit) {
         this.baseURL = baseURL ?? "";
         this.defaultOptions = { ...defaultOptions };
@@ -81,9 +110,18 @@ export class RESTClient {
             throw new RESTError(req, res);
         }
 
+        if (req.method === "HEAD") {
+            return undefined as T;
+        }
+
         return await res.json();
     }
 
+    /**
+     * construct a request URL.
+     * the URL is constructed the resource path `path` and search parameters
+     * `params` appended to `baseURL` of the client.
+     */
     #createRequestURL(path: string, params?: URLSearchParams): URL {
         const url = new URL(path, this.baseURL);
         url.search = params?.toString() ?? "";
@@ -91,6 +129,9 @@ export class RESTClient {
         return url;
     }
 
+    /**
+     * send a GET request
+     */
     // deno-lint-ignore no-explicit-any
     async get<T = any>(
         path: string,
@@ -100,15 +141,20 @@ export class RESTClient {
         return await this.#send("GET", path, params, undefined, options);
     }
 
-    // deno-lint-ignore no-explicit-any
-    async head<T = any>(
+    /**
+     * send a HEAD request
+     */
+    async head(
         path: string,
         params?: URLSearchParams,
         options?: RequestInit,
-    ): Promise<T> {
+    ): Promise<void> {
         return await this.#send("HEAD", path, params, undefined, options);
     }
 
+    /**
+     * send a POST request
+     */
     // deno-lint-ignore no-explicit-any
     async post<T = any, U = any>(
         path: string,
@@ -119,6 +165,9 @@ export class RESTClient {
         return await this.#send("POST", path, params, data, options);
     }
 
+    /**
+     * send a PUT request
+     */
     // deno-lint-ignore no-explicit-any
     async put<T = any, U = any>(
         path: string,
@@ -129,6 +178,11 @@ export class RESTClient {
         return await this.#send("PUT", path, params, data, options);
     }
 
+    /**
+     * send a DELETE request
+     *
+     * Note: request body may be ignored by server.
+     */
     // deno-lint-ignore no-explicit-any
     async delete<T = any, U = any>(
         path: string,
@@ -139,6 +193,9 @@ export class RESTClient {
         return await this.#send("DELETE", path, params, data, options);
     }
 
+    /**
+     * send a PATCH request
+     */
     // deno-lint-ignore no-explicit-any
     async patch<T = any, U = any>(
         path: string,
@@ -149,5 +206,44 @@ export class RESTClient {
         return await this.#send("PATCH", path, params, data, options);
     }
 
-    // TODO: connect, options, trace
+    /**
+     * send an OPTIONS request
+     *
+     * Note: request body may be ignored by server.
+     */
+    // deno-lint-ignore no-explicit-any
+    async options<T = any, U = any>(
+        path: string,
+        params?: URLSearchParams,
+        data?: U,
+        options?: RequestInit,
+    ): Promise<T> {
+        return await this.#send("OPTIONS", path, params, data, options);
+    }
+
+    /**
+     * send a CONNECT request
+     */
+    // deno-lint-ignore no-explicit-any
+    async connect<T = any>(
+        path: string,
+        params?: URLSearchParams,
+        options?: RequestInit,
+    ): Promise<T> {
+        return await this.#send("CONNECT", path, params, undefined, options);
+    }
+
+    /**
+     * send a TRACE request
+     *
+     * Note: insecure, usually disallowed by server.
+     */
+    // deno-lint-ignore no-explicit-any
+    async trace<T = any>(
+        path: string,
+        params?: URLSearchParams,
+        options?: RequestInit,
+    ): Promise<T> {
+        return await this.#send("TRACE", path, params, undefined, options);
+    }
 }
